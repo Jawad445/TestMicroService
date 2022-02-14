e3a1ea793357ca20f87714c7bc7e5e18c25dead1
Syed Jawad Hassan Naqvi <jhassan445@outlook.com>
2022-02-14 17:31:52 +0500

    Update controller

--- PlatformService/Controllers/PlatformController.cs
+++ PlatformService/Controllers/PlatformController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.DTOs;
+using PlatformService.Models;
 
 namespace PlatformService.Controller;
 
@@ -24,5 +25,25 @@ public class PlatformController : ControllerBase
         Console.WriteLine("Generating Data");
         var platforms = _repo.GetAll();
         return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<PlatformReadDto> GetById(int id)
+    {
+        var platform = _repo.GetById(id);
+        if(platform is not null)
+        {
+            return Ok(_mapper.Map<PlatformReadDto> (platform));
+        }
+        return NotFound();        
+    }
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto model)
+    {
+        var platformModel = _mapper.Map<Platform>(model);
+        _repo.CreatePlatform(platformModel);
+        _repo.SaveChanges();
+
+        var returnobj = _mapper.Map<PlatformReadDto>(platformModel);
+        return CreatedAtRoute(nameof(GetById), new {id = platformModel.Id}, returnobj);
     }
 }
